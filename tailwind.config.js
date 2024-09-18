96f4cfff6fc581ddca48639cf4946d44cf1465e3
SonKkc <soncb.xyz@gmail.com>
2024-09-18 16:08:22 +0700

    Update responsive breakpoints in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,20 @@ export default {
   content: ["*"],
   theme: {
     screens: {
-      'xs': '390px',
-      'xs2': '640px',
-      ...defaultTheme.screens,
+      'sm': {'min': '0px', 'max': '860px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '861px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': {'min': '1536px'},
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       colors: {
@@ -15,7 +26,7 @@ export default {
         'tertiary': '#333B7C',
         'menu_desktop': '#FEFEFE',
         'menu_mobile': '#202020',
-
+        'gray80': '#343434'
       },
     },
   },
